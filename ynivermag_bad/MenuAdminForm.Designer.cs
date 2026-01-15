87db8439e590151efe8286ca51946efa1bff1c9c
Deryabina <violettaderabina734@gmail.com>
2026-01-15 22:16:48 +0300

    Форма админа

--- ynivermag_bad/MenuAdminForm.Designer.cs
+++ ynivermag_bad/MenuAdminForm.Designer.cs
@@ -28,12 +28,87 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Lists = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Exit = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 30.25F);
+            this.label1.Location = new System.Drawing.Point(345, 3);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(228, 47);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Универмаг";
+            // 
+            // Lists
+            // 
+            this.Lists.BackColor = System.Drawing.Color.GreenYellow;
+            this.Lists.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Lists.Location = new System.Drawing.Point(717, 153);
+            this.Lists.Name = "Lists";
+            this.Lists.Size = new System.Drawing.Size(202, 72);
+            this.Lists.TabIndex = 1;
+            this.Lists.Text = "Справочники";
+            this.Lists.UseVisualStyleBackColor = false;
+            // 
+            // panel1
+            // 
+            this.panel1.Location = new System.Drawing.Point(12, 153);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(699, 365);
+            this.panel1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 24);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Роль";
+            // 
+            // Exit
+            // 
+            this.Exit.BackColor = System.Drawing.Color.LimeGreen;
+            this.Exit.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.Exit.Location = new System.Drawing.Point(717, 443);
+            this.Exit.Name = "Exit";
+            this.Exit.Size = new System.Drawing.Size(202, 75);
+            this.Exit.TabIndex = 4;
+            this.Exit.Text = "Выйти из аккаунта";
+            this.Exit.UseVisualStyleBackColor = false;
+            // 
+            // MenuAdminForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(11F, 24F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "MenuAdminForm";
+            this.ClientSize = new System.Drawing.Size(931, 545);
+            this.Controls.Add(this.Exit);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.Lists);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F);
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "MenuAdminForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Меню администратора";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button Lists;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Exit;
     }
 }
